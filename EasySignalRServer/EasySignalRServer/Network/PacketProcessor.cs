cc4dbedaa7029dfa21c99b3afb5efa00ffab9ea9
garambaek <bgr1017.kr@gmail.com>
2024-01-05 00:58:17 +0900

    test code 추가

--- EasySignalRServer/EasySignalRServer/Network/PacketProcessor.cs
+++ EasySignalRServer/EasySignalRServer/Network/PacketProcessor.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Protocols.Packets;
 using Microsoft.AspNet.SignalR.Hubs;
+using RLog;
 
 namespace EasySignalRServer.Network
 {
@@ -11,11 +12,14 @@ namespace EasySignalRServer.Network
         
         public void SendPacket(SendPacket sendPacket)
         {
-            //Clients.Caller.Invoke(sendPacket.MethodName, sendPacket);
+            Clients.Caller.Invoke(sendPacket.MethodName, sendPacket);
 
             var broadCastPacket = new BroadCastPacket(sendPacket.Nickname, sendPacket.Message);
 
             Clients.Group(ChatGroupName).Invoke(broadCastPacket.MethodName, broadCastPacket);
+
+            RLogger.Debug($"Sender:{broadCastPacket.Sender}, Message:{broadCastPacket.Message}");
+
         }
 
         // 브로드캐스트 
